082a3bf7fc280613df75a6291467fc0c0c4a3cdb
Rajkishor Patra <53007802+imraj569@users.noreply.github.com>
2023-05-31 16:13:25 +0530

    Add files via upload

--- ftp_manager.py
+++ ftp_manager.py
@@ -1,39 +1,45 @@
-#this is the source file you can modify anything you want
 import os
-from keyboard import press_and_release
-from time import sleep
-from pyautogui import write
+import time
 import colorama
 from colorama import Fore
+from keyboard import press_and_release
+from pyautogui import write
+import re
 colorama.init(autoreset=True)
-import sys
 
+def ftp_clint():
+    print(Fore.CYAN + '''
+    ╭━━━┳━━━━┳━━━╮
+    ┃╭━━┫╭╮╭╮┃╭━╮┃
+    ┃╰━━╋╯┃┃╰┫╰━╯┃
+    ┃╭━━╯╱┃┃╱┃╭━━╯
+    ┃┃╱╱╱╱┃┃╱┃┃
+    ╰╯╱╱╱╱╰╯╱╰╯         
+                    MANAGER V-1.5
+    ------------------------------------------             
+    github - https://github.com/imraj569/ftpmanager
+    instagram - @im.raj.569
+    ------------------------------------------''')
+    print(Fore.RED + '''Make sure your FTP server port is 2221
+    ------------------------------------------''')
 
-print(Fore.CYAN+'''
-╭━━━┳━━━━┳━━━╮
-┃╭━━┫╭╮╭╮┃╭━╮┃
-┃╰━━╋╯┃┃╰┫╰━╯┃
-┃╭━━╯╱┃┃╱┃╭━━╯
-┃┃╱╱╱╱┃┃╱┃┃
-╰╯╱╱╱╱╰╯╱╰╯         
-                MANAGER V-0.1
-------------------------------------------             
-                github - https://github.com/imraj569/ftpmanager
-                instagram - @im.raj.569
-------------------------------------------''')
-print(Fore.RED+'''make sure your ftp server port is 2221
-------------------------------------------''')
-os.system("start cmd /k ipconfig")
-add = input(Fore.BLUE+'Just copy and past default getway address here and press Enter: ')
-print(Fore.LIGHTYELLOW_EX+'please wait opening FTP server')
-os.system('taskkill /f /im cmd.exe')
-sleep(0.10)
-press_and_release('win+e')
-sleep(1)
-press_and_release('alt+d')
-sleep(0.10)
-write(f'ftp://{add}:2221/')
-sleep(0.10)
-press_and_release('enter')
-print(Fore.BLUE+'succesfully open ftp server')
-sys.exit()
+    output = os.popen("ipconfig").read()
+    default_gateway_match = re.search(r"Default Gateway .*?:\s+(\S+)", output)
+    if default_gateway_match:
+        default_gateway_address = default_gateway_match.group(1)
+    else:
+        print(Fore.RED + "Failed to retrieve default gateway address.")
+        exit()
+
+    print(Fore.LIGHTYELLOW_EX + 'Please wait, opening FTP server...')
+    time.sleep(0.10)
+    press_and_release('win+e')
+    time.sleep(1)
+    press_and_release('alt+d')
+    time.sleep(0.20)
+    write(f'ftp://{default_gateway_address}:2221/')
+    time.sleep(0.20)
+    press_and_release('enter')
+    print(Fore.BLUE + 'Successfully opened FTP server')
+if __name__ == "__main__":
+    ftp_clint()
